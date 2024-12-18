644b2d7f1e90a08ba5c2e90111ad57ff814caae0
Manish-Tamang <maneshtamang833@gmail.com>
2024-12-18 20:11:25 +0545

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Github Profile viewer',
+  description: 'A github user API based project made in nextjs with bolt.new and Shadcn UI',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
